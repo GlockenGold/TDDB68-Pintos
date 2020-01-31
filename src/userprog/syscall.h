d84f9b4d0cc39093f2b3e8785aa48c31e33ae0e2
Erik Halvarsson <eriha353@student.liu.se>
2020-01-31 15:34:44 +0100

    lagt till alla syscalls för lab 1 i userprog/syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -3,4 +3,18 @@
 
 void syscall_init (void);
 
+void halt(void);
+
+bool create(const char *file, unsigned initial_size);
+
+int open(const char *file);
+
+void close(int fd);
+
+int read(int fd, void *buffer, unsigned size);
+
+int write(fd, const void *buffer, unsigned size);
+
+void exit(int status);
+
 #endif /* userprog/syscall.h */
