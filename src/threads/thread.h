a4ea0f14e75974f6c15e7b24c2eda2bc23660059
Erik Halvarsson <eriha353@student.liu.se>
2020-02-05 10:54:51 +0100

    tog bort = NULL i fdtable

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -95,7 +95,7 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
-    struct file *fdtable[128] = NULL;
+    struct file *fdtable[128];
     /* Lägg in file descriptor table här*/
 #endif
 
